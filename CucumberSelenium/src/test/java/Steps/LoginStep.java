6ca59e3c3d2e506aad057bdc50769bda55b072b1
npdduy21890 <43669262+npdduy21890@users.noreply.github.com>
2019-07-08 11:10:41 +0700

    update final

--- CucumberSelenium/src/test/java/Steps/LoginStep.java
+++ CucumberSelenium/src/test/java/Steps/LoginStep.java
@@ -12,31 +12,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class LoginStep {
     WebDriver driver;
     @Given("I open browser and navigate to BC-Riboon site")
-    public void open_browser_navigate_to_homepage(){
+    public void open_browser_navigate_to_homepage (){
         System.setProperty("webdriver.chrome.driver","C:/Users/Admin/Downloads/Selenium_Cucumber_JarFile/chromedriver.exe");
-        driver.manage().window().maximize();
         driver = new ChromeDriver();
         driver.get("http://52.24.54.200/ecard-webapp/login");
         System.out.println("This step open browser");
     }
     @And("I input valid account and password")
     public void input_valid_data () {
-        WebElement account = driver.findElement(By.className(""));
-        account.sendKeys();
-        WebElement password = driver.findElement(By.className());
-        password.sendKeys();
+        WebElement account = driver.findElement(By.name("j_username"));
+        account.sendKeys("qa.qccheck+20181227@gmail.com");
+        WebElement password = driver.findElement(By.name("j_password"));
+        password.sendKeys("testtest1234");
         System.out.println("This step input account and password");
     }
     @When("I click Login button")
     public void click_button () {
-        WebElement lgbButton = driver.findElement();
+        WebElement lgbButton = driver.findElement(By.id("btnLogin"));
         lgbButton.click();
         System.out.println("This step click Login button");
     }
     @Then("Login page successfully")
     public void result () {
         System.out.println("This step open Homepage");
-        driver.get("");
+        driver.get("http://52.24.54.200/ecard-webapp/manager/home");
         driver.close();
     }
 }
