8adf54e83372835418c5fd906a5b44d0277039e5
npdduy21890 <43669262+npdduy21890@users.noreply.github.com>
2019-07-09 10:59:34 +0700

    up new

--- CucumberSelenium/Features/LoginFailed.feature
+++ CucumberSelenium/Features/LoginFailed.feature
@@ -1,9 +1,9 @@
 Feature: Login failed
   Scenario Outline: User can not login Homepage by input invalid data
     Given I open browser and navigate to BC-Riboon site
-    Given I input User Name as <account> and Password as <password>
+    Given I input User Name as "<account>" and Password as "<password>"
     When I click Login button
-    Then Site show content as <result>
+    Then Site show content as "<result>"
     Examples:
     | account | password| result                                          |
     | abcd    | badsđ   | ID、またはパスワードが正しくありません。ご確認ください |
--- CucumberSelenium/src/test/java/Steps/LoginStep.java
+++ CucumberSelenium/src/test/java/Steps/LoginStep.java
@@ -8,37 +8,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class LoginStep {
     WebDriver driver;
+
     @Given("I open browser and navigate to BC-Riboon site")
     public void open_browser_navigate_to_homepage (){
         System.setProperty("webdriver.chrome.driver","C:/Users/Admin/Downloads/Selenium_Cucumber_JarFile/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.get("http://52.24.54.200/ecard-webapp/login");
         System.out.println("This step open browser");
     }
+
     @Given("I input valid account and password")
     public void input_valid_data () {
         WebElement account = driver.findElement(By.name("j_username"));
         account.sendKeys("qa.qccheck+20181227@gmail.com");
         WebElement password = driver.findElement(By.name("j_password"));
-        password.sendKeys("testtest1234");
-        System.out.println("This step input account and password");
+        password.sendKeys("testtest1234");System.out.println("This step input account and password");
     }
-    @Given("I input User Name as <account> and Password as <password>")
+
+    @Given("I input User Name as \"([^\"]*)\" and Password as \"([^\"]*)\"")
     public void input_invalid_data (){
         WebElement account = driver.findElement(By.name("j_username"));
         account.sendKeys();
         WebElement password = driver.findElement(By.name("j_password"));
         password.sendKeys();
     }
+
     @When("I click Login button")
     public void click_button () {
         WebElement lgbButton = driver.findElement(By.id("btnLogin"));
         lgbButton.click();
         System.out.println("This step click Login button");
     }
+
     @Then("Login page successfully")
     public void result () {
         System.out.println("This step open Homepage");
@@ -46,23 +53,12 @@ public class LoginStep {
         driver.close();
     }
 
-    @Then("Site show content as <result>")
+    @Then("Site show content as \"([^\"]*)\"")
     public void result_failed () {
         System.out.println("This step dont open Homepage");
         driver.get("http://52.24.54.200/ecard-webapp/login?error=true");
         WebElement message = driver.findElement(By.className("alert alert-danger alert-dismissable"));
         message.getText();
-        try {
-            if (message.equals()) {
-                System.out.println("The content is right");
-            }
-            else {
-                System.out.println("The content is wrong");
-            }
-        } catch (Exception ex){
-            return;
-        }
-
         driver.close();
     }
 }
